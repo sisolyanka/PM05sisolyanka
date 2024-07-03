d065ef5816b030fbb5e79fd353c66c9ee0c15153
sisolyanka <150651315+sisolyanka@users.noreply.github.com>
2024-07-03 11:57:17 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Личный кабинет</title>
+</head>
+<body>
+  <h1>Авторизация</h1>
+  <form action="login.php" method="post">
+    <label for="login">Логин:</label>
+    <input type="text" name="login" id="login" required><br><br>
+
+    <label for="password">Пароль:</label>
+    <input type="password" name="password" id="password" required><br><br>
+
+    <button type="submit">Войти</button>
+  </form>
+</body>
+</html>
